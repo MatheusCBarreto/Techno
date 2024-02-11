b38114fc42995dc67faacada45752e83af0b3b8c
MatheusCBarreto <devmattbarreto@protonmail.com>
2024-02-11 17:16:00 -0300

    modal feito

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const vm = new Vue({
   el: '#app',
   data: {
     produtos: [],
+    produto: false,
   },
+  filters: {},
   methods: {
     fetchProdutos() {
       fetch('./api/produtos.json')
         .then((r) => r.json())
         .then((r) => {
           this.produtos = r;
+        });
+    },
+    numeroPreco(value) {
+      return value.TolocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+      });
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((res) => res.json())
+        .then((r) => {
+          this.produto = r;
         });
     },
   },
